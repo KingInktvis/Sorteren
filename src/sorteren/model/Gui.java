1179782b9190a9559a3884b5427a52136f0445ca
KingInktvis <rikdevries253@hotmail.nl>
2017-02-28 14:26:45 +0100

    Icon added

--- src/sorteren/model/Gui.java
+++ src/sorteren/model/Gui.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -19,6 +20,7 @@ public class Gui extends Application{
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../view/main.fxml"));
         primaryStage.setTitle("Sorteren");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("../res/stackoverflowicon.png")));
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
