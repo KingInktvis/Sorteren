a7d933d864c87af6bffc5f49d470700a2e60c1e7
KingInktvis <rikdevries253@hotmail.nl>
2017-02-28 12:18:38 +0100

    Added the EventHandeler class.

---
+++ src/sorteren/model/EventHandeler.java
@@ -0,0 +1,18 @@
+package sorteren.model;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by rik on 28-2-17.
+ */
+public class EventHandeler implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+}
