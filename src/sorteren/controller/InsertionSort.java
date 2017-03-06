566462ae6313116e0203c197a1e20cd3ffd4c989
KingInktvis <rikdevries253@hotmail.nl>
2017-03-06 14:41:31 +0100

    Added controller and view for insertion sort.

---
+++ src/sorteren/controller/InsertionSort.java
@@ -0,0 +1,12 @@
+package sorteren.controller;
+
+/**
+ * Created by rik on 6-3-17.
+ */
+public class InsertionSort extends ChartTabController {
+
+    @Override
+    public void nextStep() {
+        super.nextStep();
+    }
+}
